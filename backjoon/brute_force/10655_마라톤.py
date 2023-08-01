acc714d1ab3591350418ca977a46a754432f12c6
Ji Hwan <g1767g@naver.com>
2023-08-01 23:26:26 +0900

    feat : 48일차 문제 풀이

--- backjoon/brute_force/10655_마라톤.py
+++ backjoon/brute_force/10655_마라톤.py
@@ -4,25 +4,17 @@ arr = []
 
 for _ in range(n):
     arr.append(list(map(int, input().split())))
-my_x = arr[0][0]
-my_y = arr[0][1]
-x = arr[0][0]
-y = arr[0][1]
+
+gap = []
+for i in range(1, len(arr)):
+    sub = abs(arr[i][0] - arr[i-1][0]) + abs(arr[i][1] - arr[i-1][1])
+    gap.append(sub)
+
 result = []
-
-for i in range(1,len(arr)-1):
-    length = 0
-    for j in range(n):
-        if j == i:
-            continue
-
-        temp_x = arr[j][0]
-        temp_y = arr[j][1]
-        length += abs(temp_x - x) + abs(temp_y - y)
-        x = temp_x
-        y = temp_y
-
+sum_gap = sum(gap)
+for i in range(1,len(arr) - 1):
+    length = sum_gap
+    length -= gap[i-1] + gap[i]
+    length += abs(arr[i-1][0] - arr[i+1][0]) + abs(arr[i-1][1] - arr[i+1][1])
     result.append(length)
-    x = my_x
-    y = my_y
 print(min(result))
