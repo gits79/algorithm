3f8096a77cd4f5f2fc0d3fcc53b71702be7cac2a
Ji Hwan <g1767g@naver.com>
2023-09-11 23:06:46 +0900

    feat : 79일차 문제 풀이

---
+++ backjoon/prefix_sum/11660_구간합구하기5.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+
+arr = []
+arr.append([0] * (n + 1))
+for _ in range(n):
+    temp = list(map(int, input().split()))
+    temp.insert(0, 0)
+    arr.append(temp)
+
+prefix_sum = [[0] * (n + 1) for i in range(n + 1)]
+
+# 가로
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        prefix_sum[i][j] = prefix_sum[i][j - 1] + arr[i][j]
+
+# 세로
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        prefix_sum[j][i] += prefix_sum[j - 1][i]
+
+for i in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(prefix_sum[x2][y2] - prefix_sum[x1 - 1][y2] - prefix_sum[x2][y1 - 1] + prefix_sum[x1 - 1][y1 - 1])
