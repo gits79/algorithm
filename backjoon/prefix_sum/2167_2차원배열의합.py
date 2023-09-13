14c09d8e0d508b3709302300b7730619af06a0ab
master <g1767g@naver.com>
2023-09-13 23:36:38 +0900

    feat : 82일차 문제 풀이

---
+++ backjoon/prefix_sum/2167_2차원배열의합.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [[0] * (m + 1)]
+
+for _ in range(n):
+    temp = [0] + list(map(int, input().split()))
+    arr.append(temp)
+
+prefix_sum = [[0] * (m + 1) for i in range(n + 1)]
+
+# 가로
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        prefix_sum[i][j] = prefix_sum[i][j - 1] + arr[i][j]
+# 세로
+for i in range(1, m + 1):
+    for j in range(1, n + 1):
+        prefix_sum[j][i] += prefix_sum[j - 1][i]
+
+k = int(input())
+for _ in range(k):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(prefix_sum[x2][y2] - prefix_sum[x1 - 1][y2] - prefix_sum[x2][y1 - 1] + prefix_sum[x1 - 1][y1 - 1])
