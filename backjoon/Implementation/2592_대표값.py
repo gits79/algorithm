4c7afeb875b4b540c6036cfebe77b1e4022095a3
master <g1767g@naver.com>
2023-10-15 23:08:12 +0900

    feat : 113일차 문제 풀이

---
+++ backjoon/Implementation/2592_대표값.py
@@ -0,0 +1,15 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+arr = []
+
+for i in range(10):
+    arr.append(int(input()))
+
+num = [0] * 1001
+
+for i in arr:
+    num[i] += 1
+
+print(sum(arr) // 10)
+print(num.index(max(num)))
