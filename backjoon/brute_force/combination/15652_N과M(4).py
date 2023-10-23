d9d218c0c619de4b99f3ba23e272c416974e1395
master <g1767g@naver.com>
2023-10-23 23:47:36 +0900

    feat : 121일차 문제 풀이

---
+++ backjoon/brute_force/combination/15652_N과M(4).py
@@ -0,0 +1,19 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+n, m = map(int, input().split())
+
+selected = [0] * m
+
+
+def recur(cur, start):
+    if cur == m:
+        print(*selected)
+        return
+
+    for i in range(start, n + 1):
+        selected[cur] = i
+        recur(cur + 1, i)
+
+
+recur(0, 1)
