fba96acc84e9df4a508a0a832f92da0c0e0a7336
master <g1767g@naver.com>
2023-12-12 23:45:08 +0900

    feat : 167일차 문제 풀이

---
+++ backjoon/Implementation/1094_막대기.py
@@ -0,0 +1,15 @@
+import sys
+input = lambda : sys.stdin.readline().strip()
+
+x = int(input())
+count = 0
+n = 64
+while x > 0:
+	if n > x:
+		n = n // 2
+	else:
+		count += 1
+		x -= n
+print(count)
+
+
