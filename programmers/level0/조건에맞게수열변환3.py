6c040358a52db929757dade2933359a912c3c22e
master <g1767g@naver.com>
2023-12-21 19:03:32 +0900

    feat : 176일차 문제 풀이

---
+++ programmers/level0/조건에맞게수열변환3.py
@@ -0,0 +1,9 @@
+def solution(arr, k):
+    answer = []
+
+    for i in arr:
+        if k % 2 == 0:
+            answer.append(i + k)
+        else:
+            answer.append(i * k)
+    return answer
