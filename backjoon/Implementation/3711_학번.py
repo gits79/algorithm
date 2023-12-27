a4ef37cf28d62e3a7ffc2f9c764819c41ee4fcf8
master <g1767g@naver.com>
2023-12-27 23:28:43 +0900

    feat : 182일차 문제 풀이

---
+++ backjoon/Implementation/3711_학번.py
@@ -0,0 +1,25 @@
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+t = int(input())
+for tc in range(t):
+    n = int(input())
+    arr = []
+    result = 0
+    for _ in range(n):
+        arr.append(int(input()))
+
+    i = 1
+    while True:
+        temp = []
+        for j in arr:
+            temp.append(j % i)
+
+        if len(temp) == len(set(temp)):
+            result = i
+            break
+
+        i += 1
+
+    print(i)
