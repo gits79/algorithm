665eaa3860088430601de7c133916b26afcee458
김지환 <101376845+gits79@users.noreply.github.com>
2024-01-02 23:34:41 +0900

    feat: 185일차 문제 풀이

---
+++ backjoon/Implementation/2438_별찍기.py
@@ -0,0 +1,10 @@
+inp = int(input())
+for i in range(1,(inp+1)):
+    print("*" * i)
+ 
+'''
+inp = int(input())
+for i in range(1, (inp+1)):
+    for j in range(1, (i+1)):
+        print("*",end="")
+    print()
