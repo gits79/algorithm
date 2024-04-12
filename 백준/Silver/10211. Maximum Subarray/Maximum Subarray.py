a3dc19fecb194571f03f35f2b815d79350b53956
김지환 <101376845+gits79@users.noreply.github.com>
2024-04-12 22:04:56 +0900

    [Silver IV] Title: Maximum Subarray, Time: 120 ms, Memory: 110404 KB -BaekjoonHub

---
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.py
@@ -0,0 +1,12 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+t = int(input())
+for tc in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    for i in range(1, n):
+        arr[i] = max(arr[i], arr[i - 1] + arr[i])
+
+    print(max(arr))
