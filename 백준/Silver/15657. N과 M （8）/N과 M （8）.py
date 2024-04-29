64ff069d67b34857b2ea6a840df3a18fd0feeb70
김지환 <101376845+gits79@users.noreply.github.com>
2024-04-29 21:13:55 +0900

    [Silver III] Title: N과 M (8), Time: 164 ms, Memory: 112948 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,18 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+selected = [0] * m
+def recur(cur, start):
+    if cur == m:
+        print(*selected)
+        return
+
+    for i in range(start, n):
+        selected[cur] = arr[i]
+        recur(cur + 1, i)
+
+recur(0, 0)
