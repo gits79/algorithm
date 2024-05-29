9880fdcb352241df0f62d1f62ac1a54285d1e1b4
김지환 <101376845+gits79@users.noreply.github.com>
2024-05-29 23:32:06 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,12 @@
+n, k = map(int,input().split())
+queue = [i for i in range(1,n+1)]
+
+result = []
+count = 0
+
+for i in range(n):
+    count += k-1
+    if count >= len(queue):
+        count = count%len(queue)
+    result.append(str(queue.pop(count)))
+print("<", ", ".join(result), ">", sep = '')
