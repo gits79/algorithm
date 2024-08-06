f5f2185476676ddb327554ece3615da8294a4286
김지환 <101376845+gits79@users.noreply.github.com>
2024-08-06 17:25:37 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -2,22 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static int[] arr;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		arr = new int[N+1];
 		arr[0] = 1;
-		int result = fac(N) / fac(K) / fac(N-K);
-		bw.write(Integer.toString(result));
-		bw.flush();
-		bw.close();
-		br.close();
+		
+		System.out.println(fac(N) / fac(K) / fac(N-K));
 	}
 	
-	static int[] arr;
 
 	static int fac (int n) {
 		if( n < 2) {
