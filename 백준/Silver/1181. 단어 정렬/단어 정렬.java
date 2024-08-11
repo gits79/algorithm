b170d9aa124d93e39c7ed976b606d0842a56e2f1
김지환 <101376845+gits79@users.noreply.github.com>
2024-08-11 17:56:36 +0900

    [Silver V] Title: 단어 정렬, Time: 2120 ms, Memory: 28616 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,33 +1,41 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		Set<String> set = new HashSet<String>();
-		
-		for(int i = 0 ; i < N ; i++) {
-			set.add(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		ArrayList<String> arr = new ArrayList<>();
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			String str = st.nextToken();
+			if (!arr.contains(str))
+				arr.add(str);
 		}
-		
-		List<String> list = new ArrayList<String>();
-		list.addAll(set);
-		Collections.sort(list, (o1, o2)->{
-			int len = o1.length() - o2.length();
-			if(len == 0) {
-				return o1.compareTo(o2);
-			}
-			
-			return len;} );
-		
-		for(String s : list) sb.append(s).append("\n");
-		
-		br.close();
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
+
+		Collections.sort(arr, (o1, o2) -> {
+			int lengthCompare = Integer.compare(o1.length(), o2.length());
+			if (lengthCompare != 0)
+				return lengthCompare;
+
+			return o1.compareTo(o2);
+		});
+
+		for (String s : arr)
+			System.out.println(s);
+
 	}
+
 }
