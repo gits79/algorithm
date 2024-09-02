7d2cbd083cf892cc325c2ecacabebfc29ce552e5
김지환 <101376845+gits79@users.noreply.github.com>
2024-09-02 23:45:32 +0900

    [Silver I] Title: 영역 구하기, Time: 72 ms, Memory: 13112 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,75 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
+	
+	static int M, N, K, cnt;
+	static int[][] arr;
+	static boolean[][] v;
+	static ArrayList<Integer> lst = new ArrayList<>();
+	
+	static final int[] di = {-1, 0, 1, 0};
+	static final int[] dj = {0, 1, 0, -1};
+	
+	
+	static void dfs(int i, int j) {
+		v[i][j] = true;
+		cnt++;
+		
+		for(int d = 0; d < 4; d++) {
+			int ni = i + di[d];
+			int nj = j + dj[d];
+			if(0 <= ni && ni < M && 0 <= nj && nj < N && !v[ni][nj] && arr[ni][nj] == 0) {
+				dfs(ni, nj);
+			}
+		}
+		
+		
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		cnt = 0;
+		arr = new int[M][N];
+		v = new boolean[M][N];
+		
+		for(int l = 0; l < K; l++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			
+			for(int i = x1; i < x2; i++) {
+				for(int j = y1; j < y2; j++) {
+					arr[i][j] = 1;
+				}
+			}			
+		}
+		
+		for(int i = 0; i < M; i++) {
+			for(int j = 0; j < N; j++) {
+				cnt = 0;
+				if(!v[i][j] && arr[i][j] == 0)
+					dfs(i,j);
+				if(cnt != 0) {
+					lst.add(cnt);
+				}
+			}
+		}
+		
+		Collections.sort(lst);
+		System.out.println(lst.size());
+		for(Integer n : lst) System.out.print(n + " ");
+		
 
-    static final int[] di = {-1, 0, 1, 0};
-    static final int[] dj = {0, 1, 0, -1};
-    static int count = 0;
-
-    static void dfs(int[][] arr, int i, int j) {
-        int width = arr[0].length;
-        int height = arr.length;
-        arr[i][j] = -1;
-        count++;
-
-        for (int k = 0; k < 4; k++) {
-            int ni = i + di[k];
-            int nj = j + dj[k];
-            if (ni >= 0 && ni < height && nj >= 0 && nj < width && arr[ni][nj] == 0) {
-                dfs(arr, ni, nj);
-
-            }
-        }
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-
-        int[][] arr = new int[n][m];
-
-        for (int i = 0; i < k; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-
-            for (int w = x1; w < x2; w++) {
-                for (int h = y1; h < y2; h++) {
-                    arr[h][w] = 1;
-                }
-            }
-        }
-
-
-        ArrayList<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (arr[i][j] == 0) {
-                    count = 0;
-                    dfs(arr, i, j);
-                    answer.add(count);
-                }
-            }
-        }
-        Collections.sort(answer);
-        System.out.println(answer.size());
-        for (int i = 0; i < answer.size(); i++)
-            System.out.print(answer.get(i) + " ");
-
-    }
+	}
 }
