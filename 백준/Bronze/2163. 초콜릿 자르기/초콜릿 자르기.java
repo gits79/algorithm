4273c6a43a5aece4f458134fee89f1901b7de9d2
김지환 <101376845+gits79@users.noreply.github.com>
2024-10-03 21:18:07 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int result = (N - 1) + ((M - 1) * N);
+		
+		System.out.println(result);
+	}
+
+}
