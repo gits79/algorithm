39277f8ba649093bcf30b3c41b398000213b87b5
김지환 <101376845+gits79@users.noreply.github.com>
2024-11-04 17:48:59 +0900

    [Silver II] Title: 겉넓이 구하기, Time: 164 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.java
+++ 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.java
@@ -1,66 +1,53 @@
 import java.io.*;
 import java.util.*;
-
-public class Main {
-
-	private static int[] di = { -1, 0, 1, 0 };
-	private static int[] dj = { 0, -1, 0, 1 };
-
-	static int N, M, map[][];
-
-	private static boolean chkIndex(int i, int j) {
-		if (0 <= i && i < N && 0 <= j && j < M)
-			return true;
-		return false;
-	}
-
-	private static int temp(int i, int j) {
-		int cnt = 0;
-		int height = map[i][j];
-		for (int d = 0; d < 4; d++) {
-			int ni = i + di[d];
-			int nj = j + dj[d];
-			// 인덱스안에 있으면 내 높이를 기준으로 사방탐색 나보다 높이가 낮아야만 겉넓이를 가질 수 있음
-			if (chkIndex(ni, nj)) {
-				int n = height - map[ni][nj];
-				if (n > 0)
-					cnt += n;
-			}
-			// 인덱스 벗어난 경우
-			else {
-				cnt += height;
-			}
-
-		}
-
-		return cnt;
-	}
-
+public class Main{
+	
+	static int N, M;
+	static int[][] board;
+	static int area;
+	static int[] dr = {-1, 0, 1, 0}; // 상우하좌
+	static int[] dc = {0, 1, 0, -1};
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-
-		map = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
+		
+		
+		board = new int[N][M];
+		
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j=0; j < M; j++) {				
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		// 윗면넓이 + 밑면넓이 시작
+		area = 2 * N * M;
+		
+		for(int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+				int h = board[i][j];
+				for(int k = 1; k <= h; k++) { // 해당칸의 높이
+					int curh = k;
+					for(int d = 0; d < 4; d++) {
+						int rr = i + dr[d];
+						int cc = j + dc[d];
+						if(0 > rr || rr >= N || 0 > cc || cc >= M) {
+							area++;	
+						}
+						if(0 <= rr && rr < N && 0 <= cc && cc < M && curh > board[rr][cc]) {
+							area++;
+						}
+					}
+				}
 			}
 		}
 
-		int answer = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				answer += temp(i, j);
-			}
-		}
+		System.out.println(area);
 
-		answer += N * M * 2;
-
-		System.out.println(answer);
-
+		br.close();
 	}
-
 }
