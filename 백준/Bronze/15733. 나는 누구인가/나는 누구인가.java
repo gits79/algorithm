3a954a63ebf64d1102ae4a191aba9ece3a356cce
김지환 <101376845+gits79@users.noreply.github.com>
2024-12-28 23:59:13 +0900

    [Bronze V] Title: 나는 누구인가, Time: 60 ms, Memory: 11412 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        System.out.println("I'm Sexy");
+    }
+}
