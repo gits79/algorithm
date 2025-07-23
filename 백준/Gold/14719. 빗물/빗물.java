0060be45f93f5a0c63789e2f66c1672d761a9928
김지환 <101376845+gits79@users.noreply.github.com>
2025-07-23 16:00:26 +0900

    [Gold V] Title: 빗물, Time: 72 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -2,53 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int H, W;
-    static int[] arr;
-    static int lmax, rmax;
-    static int ans;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int[] arr = new int[W + 1];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i < W + 1; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int max_height = 0;
+		int[] prefix = new int[W + 2];
+		int[] suffix = new int[W + 2];
+		// 누적합
+		// 오른쪽 방향으로 진행하며 높은 위치를 기준으로 평평하게 천막을 씌우는 느낌
+		for (int i = 1; i < W + 1; i++) {
+			prefix[i] = Math.max(arr[i], prefix[i - 1]);
+		}
+		// 왼쪽 방향으로 진행하며 높은 위치를 기준으로 평평하게 천막을 씌우는 느낌
+		max_height = 0;
+		for (int i = W; i >= 0 ; i--) {
+			suffix[i] = Math.max(arr[i], suffix[i + 1]);
+		}
+		// 양쪽 두개 누적합의 최소값이 문제를 기준으로 파란색 천막의 높이
+		int sum = 0;
+		for (int i = 0; i < W; i++) {
+			sum += Math.min(prefix[i], suffix[i]) - arr[i];
+		}
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        H = Integer.parseInt(st.nextToken());
-        W = Integer.parseInt(st.nextToken());
-
-        arr = new int[W];
-        
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < W; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        ans = 0;
-        // i번째 인덱스에 물이 고일 수 있는지
-        for (int i = 1; i < W - 1; i++) {
-        	lmax = 0;
-            rmax = 0;
-            
-            // i기준 왼쪽 벽 중 가장 큰 애 찾기
-            for (int j = 0; j < i; j++) {
-                lmax = Math.max(arr[j], lmax);
-//                System.out.print("lmax: " + lmax + " ");
-
-            }
-            // i기준 오른쪽 벽 중 가장 큰 애 찾기
-            for (int k = i + 1; k < W; k++) {
-                rmax = Math.max(arr[k], rmax);
-//                System.out.print("rmax: " + rmax + " ");
-            }
-            
-            int water = Math.min(lmax, rmax) - arr[i];
-
-            if (water > 0) {
-                ans += water;
-            }
-
-        }
-
-        System.out.println(ans);
-        
-        br.close();
-    }
+		System.out.println(sum);
+		
+	}
 }
