ed65f0a15c9db407d450dad148272182c0157cc1
김지환 <101376845+gits79@users.noreply.github.com>
2025-08-10 17:11:41 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 72 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,40 +1,41 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    static int N;
+    static Stack<Character> stack;
 
-		int n = Integer.parseInt(st.nextToken());
-		String[] arr = new String[n];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			arr[i] = st.nextToken();
-		}
+        N = Integer.parseInt(st.nextToken());
+        int answer = 0;
 
-		int answer = 0;
-		Stack<Character> stack;
-		for (String str : arr) {
-			stack = new Stack<>();
-			stack.add(' ');
-			for (int i = 0; i < str.length(); i++) {
-				if (!stack.contains(str.charAt(i)) || stack.get(stack.size() - 1).equals(str.charAt(i))) {
-					stack.add(str.charAt(i));
-				} else
-					break;
-			}
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            stack = new Stack<>();
+            boolean flag = true;
+            for (int j = 0; j < str.length(); j++) {
+                char c = str.charAt(j);
+                if (stack.isEmpty()) {
+                    stack.push(c);
+                } else {
+                    if (stack.peek() != c && stack.contains(c)) {
+                        flag = false;
+                        break;
+                    } else stack.push(c);
 
-			if (str.length() == stack.size() - 1)
-				answer++;
-		}
+                }
+            }
+            if (flag) answer++;
+        }
 
-		System.out.println(answer);
-	}
+        System.out.println(answer);
 
+
+    }
 }
