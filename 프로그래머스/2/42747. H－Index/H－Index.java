966b65ca30bbe139efd1570ab308ae79e835c31e
김지환 <101376845+gits79@users.noreply.github.com>
2025-10-24 23:31:43 +0900

    [level 2] Title: H-Index, Time: 2.04 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,19 +4,32 @@ class Solution {
     public int solution(int[] citations) {
         int answer = 0;
         Arrays.sort(citations);
-        int a = 0, b = 0;
-        for(int i = 0; i < 10000; i++) {
-            for(int j = 0; j < citations.length; j++) {
-                if(citations[j] >= i) a++;
+        
+        int start = 0;
+        int end = 10001;
+        
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            
+            int a = 0;
+            int b = 0;
+            
+            for(int i = 0; i < citations.length; i++) {
+                if(citations[i] >= mid) a++;
                 else b++;
-                if(b > i) break;
             }
-            if(b <= i && a >= i) answer = i;
-            a = 0;
-            b = 0;
+            
+            if(a >= mid && b <= mid) {
+                answer = start;
+                start = mid + 1;
+            }
+            else end = mid - 1;
+            
+            
         }
+        // System.out.println(start);
+        // System.out.println(end);
         
-        
-        return answer;
+        return end;
     }
 }
