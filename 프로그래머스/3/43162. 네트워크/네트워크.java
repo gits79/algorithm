91db88db6824e6b17a8a4aed4a04d9c8006128eb
김지환 <101376845+gits79@users.noreply.github.com>
2025-10-28 19:28:53 +0900

    [level 3] Title: 네트워크, Time: 0.71 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,47 +1,39 @@
 import java.util.*;
 
 class Solution {
-    static int N, parent[];
     
-    static void init(){
-        parent = new int[N];
-        for(int i = 0; i < N; i++) parent[i] = i;
-    }
+    static int N, M;
+    static boolean[] v;
     
-    static void union(int n1, int n2) {
-        int a = find(n1);
-        int b = find(n2);
-        if(a > b) parent[b] = a;
-        else parent[a] = b;
-    }
     
-    static int find(int k) {
-        if(parent[k] != k) parent[k] = find(parent[k]);
-        return parent[k];
-    }
     
-    static boolean connected(int n1, int n2) {
-        return find(n1) == find(n2);
+    static void dfs(int i, int[][] computers) {
+        v[i] = true;
+        
+        for(int j = 0; j < M; j++) {
+            if(!v[j] && computers[i][j] == 1)
+                dfs(j, computers);
+        }
     }
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        N = n;
-        init();
+        N = computers.length;
+        M = computers[0].length;
+        v = new boolean[N];
         
-        for(int i = 0; i < computers.length; i++) {
-            for(int j = 0; j < computers[0].length; j++) {
-                if(computers[i][j] == 1) {
-                    union(i , j);
+        
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                if(computers[i][j] == 1 && !v[i]) {
+                    dfs(i , computers);
+                    answer++;
                 }
             }
         }
         
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0; i < N; i++){
-            set.add(find(i));
-        }
         
-        return set.size();
+        
+        return answer;
     }
 }
