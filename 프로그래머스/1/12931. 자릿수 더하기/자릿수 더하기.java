9592bd2170f3c1a5d81dd1da870c761398fa2cb3
김지환 <101376845+gits79@users.noreply.github.com>
2026-02-11 20:44:56 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,13 +1,12 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        while(n!=0){
+        while(n > 0) {
             answer += n % 10;
             n /= 10;
-
         }
 
         return answer;
