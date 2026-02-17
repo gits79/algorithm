189deb2b37ae0167ac4ffc64bdcd6c640a36d526
김지환 <101376845+gits79@users.noreply.github.com>
2026-02-17 14:49:44 +0900

    [level 2] Title: 방문 길이, Time: 0.23 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,54 +1,50 @@
 import java.util.*;
 
+
+
 class Solution {
     
-    static final int[] di = {-1 ,0 ,1 ,0}; // 상 우 하 좌
-    static final int[] dj = {0 ,1 ,0 ,-1};
-    static final char[] op = {'U', 'R', 'D', 'L'};
-    
-    static int x, y;
     static int N = 11;
-    static boolean[][][] v;
+static int[][] map = new int[N][N];
+static boolean[][][] v = new boolean[N][N][4];
+static final int[] di = {-1 , 0, 1, 0};
+static final int[] dj = {0 , 1, 0, -1};
+
+static boolean chkIdx(int i, int j) {
+    if(0 <= i && i < N && 0 <= j && j < N) return true;
+    return false;
+}
+
+static int dir(char c) {
+    if(c == 'U') return 0;
+    else if(c == 'R') return 1;
+    else if(c == 'D') return 2;
+    return 3;
+}
     
-    static int cnt;
-    
-    static int getDir(char c) {
-        for(int i = 0; i < 4; i++) 
-            if(c == op[i]) return i;
-        return -1;
-    }
-    
-    static void move(int dir) {
-        int nx = x + di[dir];
-        int ny = y + dj[dir];
-        if(chkIdx(nx, ny)) {
-            if(!v[x][y][dir]) {
-            cnt++;
-            v[x][y][dir] = true;
-            v[nx][ny][(dir + 2) % 4] = true;
-                
-            }
-            x = nx;
-            y = ny;
-        }
-        
-    }
-    
-    static boolean chkIdx(int i, int j){
-        return (0 <= i && i < N && 0 <= j && j < N);
-    }
     
     public int solution(String dirs) {
-        cnt = 0;
-        x = 5;
-        y = 5;
-        v = new boolean[N][N][4];
+        int answer = 0;
+        int x = 5, y = 5;
         
         for(int i = 0; i < dirs.length(); i++) {
-            int dir = getDir(dirs.charAt(i));
-            move(dir);
+            char c = dirs.charAt(i);
+            int dir = dir(c);
+            int nx = x + di[dir]; 
+            int ny = y + dj[dir]; 
+            
+            if(chkIdx(nx, ny)) {
+                if(!v[x][y][dir]) {
+                    answer++;
+                    v[x][y][dir] = true;
+                    v[nx][ny][(dir + 2) % 4] = true;
+                }
+                x = nx;
+                y = ny;
+            }
+            
         }
         
-        return cnt;
+        return answer;
     }
 }
